51b724217fe153675d929e6f93768840708bdfba
pikopikopi <popgirlchan@yahoo.co.jp>
2018-09-13 23:44:43 +0900

    first from mouse2

--- CONFIG/CONFIG.js
+++
@@ -1,8 +0,0 @@
-const DEV_CONFIG = require('./DEV_CONFIG.JS');
-// const PRD_CONFIG = require('./PRD_CONFIG.JS');
-
-const BASE_CONFIG = {};
-
-const CONFIG = Object.assign(BASE_CONFIG, DEV_CONFIG);
-
-module.exports = CONFIG;
